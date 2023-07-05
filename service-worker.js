b76d17059aa7b6a7fb791bcfe9b010ea90bb8208
Wilkor <wilkorguitarrista@msn.com>
2023-07-05 19:20:52 -0300

    Melhorias
    Adicionando modal para decisão se vai executar ou não a tarefa

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.69924795160b69dde8af747a5c4ab36f.js"
+  "./precache-manifest.07ac7bcc62ff2aa7366594541634a590.js"
 );
 
 self.addEventListener('message', (event) => {
